6766d82ff70156c78b97a5b413d2f98964db286f
PepePizza <anilar21@student.aau.dk>
2024-11-14 14:53:27 +0100

    Nu er der fisk

---
+++ Bitch ass forest/Assets/Sardine/Scripts/Sardinego.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sardinego : MonoBehaviour
+{
+    public GameObject fish;
+    public GameObject swimTarget;
+    public float speed;
+    private Vector3 poo;
+
+    private void Start()
+    {
+        poo = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fish.transform.position = Vector3.MoveTowards(fish.transform.position, swimTarget.transform.position, speed);
+        if (Vector3.Distance(transform.position, swimTarget.transform.position) < 0.2f)
+        {
+            transform.position = poo;
+        }
+    }
+}
