b158ec4c83824f319747d9d9984f6047aa55712b
Lukas kyster <kyster.lukas1508@gmail.com>
2024-11-15 11:30:51 +0100

    Position logger update

--- Bitch ass forest/Assets/Scenes/Lukas/PositionLogger.cs
+++ Bitch ass forest/Assets/Scenes/Lukas/PositionLogger.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PositionLogger : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private string filePath;
+    private Transform playerTransform;
+
     void Start()
     {
-        
+        // Define the file path for logging
+        filePath = "C:\\Users\\lukas\\OneDrive - Aalborg Universitet\\Dokumenter\\player_positions.csv";
+
+
+        // Create the file and write the header
+        File.WriteAllText(filePath, "Time,PositionX,PositionZ\n");
+
+        // Reference the main camera or player's transform
+        playerTransform = Camera.main.transform; // Adjust if you're using a custom player object
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Get the current x and z positions
+        Vector3 position = playerTransform.position;
+        string log = $"{Time.time},{position.x},{position.z}\n";
+
+        // Append the log to the file
+        File.AppendAllText(filePath, log);
     }
 }
