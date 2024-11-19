20752ea348a4ce5259b573d69e38cf464d53cd1b
Nicolai Schøler <nschol22@student.aau.dk>
2024-11-19 13:58:24 +0100

    Quality of life changes

---
+++ Bitch ass forest/Assets/Scenes/Schøler/RadioStart.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioStart : MonoBehaviour
+{
+    public AudioSource radioSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        radioSource.Play();
+        radioSource.Pause();
+    }
+}
