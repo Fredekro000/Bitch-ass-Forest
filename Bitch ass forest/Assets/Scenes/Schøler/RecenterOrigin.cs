a794e3b625dd228b38048d03fc8f74c77a78e9a0
Nicolai Schøler <nschol22@student.aau.dk>
2024-11-22 10:56:25 +0100

    Small changes + REcenter

---
+++ Bitch ass forest/Assets/Scenes/Schøler/RecenterOrigin.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.XR.CoreUtils;
+
+public class RecenterOrigin : MonoBehaviour
+{
+    public Transform head;
+    public Transform origin;
+    public Transform target;
+
+    public InputActionProperty recenterButton;
+
+    public void Recenter()
+    {
+        XROrigin xrOrigin = GetComponent<XROrigin>();
+        xrOrigin.MoveCameraToWorldLocation(target.position);
+        xrOrigin.MatchOriginUpCameraForward(target.up, target.forward);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (recenterButton.action.WasPressedThisFrame())
+        {
+            Recenter();
+        }
+    }
+}
